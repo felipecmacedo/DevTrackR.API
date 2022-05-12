4e4ace2f005920534fc9fa75e2fdc65b7a20e03c
Felipe Cimardi Macedo <felipecimardimacedo@gmail.com>
2022-05-12 12:29:35 -0300

    Aula 4 finalizada

--- Controllers/PackagesController.cs
+++ Controllers/PackagesController.cs
@@ -1,7 +1,9 @@
 using DevTrackR.API.Entities;
 using DevTrackR.API.Models;
 using DevTrackR.API.Persistence;
+using DevTrackR.API.Persistence.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevTrackR.API.Controllers
 {
@@ -9,17 +11,17 @@ namespace DevTrackR.API.Controllers
     [Route("api/packages")]
     public class PackagesController : ControllerBase
     {
-        private readonly DevTrackRContext _context;
+        private readonly IPackageRepository _repository;
 
-        public PackagesController(DevTrackRContext context)
+        public PackagesController(IPackageRepository repository)
         {
-            _context = context;
+            _repository = repository;
         }
 
         [HttpGet]
         public IActionResult GetAll()
         {
-            var packages = _context.Packages;
+            var packages = _repository.GetAll();
 
             return Ok(packages);
         }
@@ -27,7 +29,7 @@ namespace DevTrackR.API.Controllers
         [HttpGet("{code}")]
         public IActionResult GetByCode(string code)
         {
-            var package = _context.Packages.SingleOrDefault(p => p.Code == code);
+            var package = _repository.GetByCode(code);
 
             if (package == null)
             {
@@ -46,7 +48,7 @@ namespace DevTrackR.API.Controllers
             }
             var package = new Package(model.Title, model.Weight);
 
-            _context.Packages.Add(package);
+            _repository.Add(package);
 
             return CreatedAtAction("GetByCode", new { code = package.Code }, package);
         }
@@ -54,7 +56,7 @@ namespace DevTrackR.API.Controllers
         [HttpPost("{code}/updates")]
         public IActionResult PostUpdate(string code, AddPackageUpdateInputModel model)
         {
-            var package = _context.Packages.SingleOrDefault(p => p.Code == code);
+            var package = _repository.GetByCode(code);
 
             if (package == null)
             {
@@ -62,6 +64,8 @@ namespace DevTrackR.API.Controllers
             }
 
             package.AddUpdate(model.Status, model.Delivered);
+
+            _repository.Update(package);
 
             return NoContent();
         }
